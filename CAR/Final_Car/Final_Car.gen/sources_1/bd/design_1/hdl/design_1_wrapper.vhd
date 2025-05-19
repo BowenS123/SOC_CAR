d70ee1cc64f11be91810afbfa3b063ddeeade80f
BowenS123 <145456083+BowenS123@users.noreply.github.com>
2025-05-19 11:29:58 +0200

    Final car blokdesign en xsa

---
+++ CAR/Final_Car/Final_Car.gen/sources_1/bd/design_1/hdl/design_1_wrapper.vhd
@@ -0,0 +1,219 @@
+--Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
+--Copyright 2022-2024 Advanced Micro Devices, Inc. All Rights Reserved.
+----------------------------------------------------------------------------------
+--Tool Version: Vivado v.2024.1.2 (win64) Build 5164865 Thu Sep  5 14:37:11 MDT 2024
+--Date        : Mon May 19 10:46:24 2025
+--Host        : Bowen-Laptop running 64-bit major release  (build 9200)
+--Command     : generate_target design_1_wrapper.bd
+--Design      : design_1_wrapper
+--Purpose     : IP block netlist
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+library UNISIM;
+use UNISIM.VCOMPONENTS.ALL;
+entity design_1_wrapper is
+  port (
+    DDR_addr : inout STD_LOGIC_VECTOR ( 14 downto 0 );
+    DDR_ba : inout STD_LOGIC_VECTOR ( 2 downto 0 );
+    DDR_cas_n : inout STD_LOGIC;
+    DDR_ck_n : inout STD_LOGIC;
+    DDR_ck_p : inout STD_LOGIC;
+    DDR_cke : inout STD_LOGIC;
+    DDR_cs_n : inout STD_LOGIC;
+    DDR_dm : inout STD_LOGIC_VECTOR ( 3 downto 0 );
+    DDR_dq : inout STD_LOGIC_VECTOR ( 31 downto 0 );
+    DDR_dqs_n : inout STD_LOGIC_VECTOR ( 3 downto 0 );
+    DDR_dqs_p : inout STD_LOGIC_VECTOR ( 3 downto 0 );
+    DDR_odt : inout STD_LOGIC;
+    DDR_ras_n : inout STD_LOGIC;
+    DDR_reset_n : inout STD_LOGIC;
+    DDR_we_n : inout STD_LOGIC;
+    DIRECTIONS0_tri_o : out STD_LOGIC_VECTOR ( 1 downto 0 );
+    DIRECTIONS1_tri_o : out STD_LOGIC_VECTOR ( 1 downto 0 );
+    DIRECTIONS2_tri_o : out STD_LOGIC_VECTOR ( 1 downto 0 );
+    DIRECTIONS3_tri_o : out STD_LOGIC_VECTOR ( 1 downto 0 );
+    FIXED_IO_ddr_vrn : inout STD_LOGIC;
+    FIXED_IO_ddr_vrp : inout STD_LOGIC;
+    FIXED_IO_mio : inout STD_LOGIC_VECTOR ( 53 downto 0 );
+    FIXED_IO_ps_clk : inout STD_LOGIC;
+    FIXED_IO_ps_porb : inout STD_LOGIC;
+    FIXED_IO_ps_srstb : inout STD_LOGIC;
+    IIC_0_0_scl_io : inout STD_LOGIC;
+    IIC_0_0_sda_io : inout STD_LOGIC;
+    IIC_0_scl_io : inout STD_LOGIC;
+    IIC_0_sda_io : inout STD_LOGIC;
+    PWM0 : out STD_LOGIC;
+    PWM1 : out STD_LOGIC;
+    PWM2 : out STD_LOGIC;
+    PWM3 : out STD_LOGIC;
+    clk_in1_0 : in STD_LOGIC;
+    signal_in_0 : in STD_LOGIC;
+    signal_in_1 : in STD_LOGIC;
+    signal_in_2 : in STD_LOGIC;
+    signal_in_3 : in STD_LOGIC;
+    sonar_echo_0 : in STD_LOGIC;
+    sonar_echo_1 : in STD_LOGIC;
+    sonar_trig_0 : out STD_LOGIC;
+    sonar_trig_1 : out STD_LOGIC
+  );
+end design_1_wrapper;
+
+architecture STRUCTURE of design_1_wrapper is
+  component design_1 is
+  port (
+    DDR_cas_n : inout STD_LOGIC;
+    DDR_cke : inout STD_LOGIC;
+    DDR_ck_n : inout STD_LOGIC;
+    DDR_ck_p : inout STD_LOGIC;
+    DDR_cs_n : inout STD_LOGIC;
+    DDR_reset_n : inout STD_LOGIC;
+    DDR_odt : inout STD_LOGIC;
+    DDR_ras_n : inout STD_LOGIC;
+    DDR_we_n : inout STD_LOGIC;
+    DDR_ba : inout STD_LOGIC_VECTOR ( 2 downto 0 );
+    DDR_addr : inout STD_LOGIC_VECTOR ( 14 downto 0 );
+    DDR_dm : inout STD_LOGIC_VECTOR ( 3 downto 0 );
+    DDR_dq : inout STD_LOGIC_VECTOR ( 31 downto 0 );
+    DDR_dqs_n : inout STD_LOGIC_VECTOR ( 3 downto 0 );
+    DDR_dqs_p : inout STD_LOGIC_VECTOR ( 3 downto 0 );
+    FIXED_IO_mio : inout STD_LOGIC_VECTOR ( 53 downto 0 );
+    FIXED_IO_ddr_vrn : inout STD_LOGIC;
+    FIXED_IO_ddr_vrp : inout STD_LOGIC;
+    FIXED_IO_ps_srstb : inout STD_LOGIC;
+    FIXED_IO_ps_clk : inout STD_LOGIC;
+    FIXED_IO_ps_porb : inout STD_LOGIC;
+    IIC_0_scl_i : in STD_LOGIC;
+    IIC_0_scl_o : out STD_LOGIC;
+    IIC_0_scl_t : out STD_LOGIC;
+    IIC_0_sda_i : in STD_LOGIC;
+    IIC_0_sda_o : out STD_LOGIC;
+    IIC_0_sda_t : out STD_LOGIC;
+    IIC_0_0_sda_i : in STD_LOGIC;
+    IIC_0_0_sda_o : out STD_LOGIC;
+    IIC_0_0_sda_t : out STD_LOGIC;
+    IIC_0_0_scl_i : in STD_LOGIC;
+    IIC_0_0_scl_o : out STD_LOGIC;
+    IIC_0_0_scl_t : out STD_LOGIC;
+    sonar_trig_0 : out STD_LOGIC;
+    sonar_trig_1 : out STD_LOGIC;
+    sonar_echo_0 : in STD_LOGIC;
+    sonar_echo_1 : in STD_LOGIC;
+    clk_in1_0 : in STD_LOGIC;
+    PWM0 : out STD_LOGIC;
+    PWM1 : out STD_LOGIC;
+    PWM2 : out STD_LOGIC;
+    PWM3 : out STD_LOGIC;
+    signal_in_0 : in STD_LOGIC;
+    signal_in_1 : in STD_LOGIC;
+    signal_in_2 : in STD_LOGIC;
+    signal_in_3 : in STD_LOGIC;
+    DIRECTIONS0_tri_o : out STD_LOGIC_VECTOR ( 1 downto 0 );
+    DIRECTIONS1_tri_o : out STD_LOGIC_VECTOR ( 1 downto 0 );
+    DIRECTIONS2_tri_o : out STD_LOGIC_VECTOR ( 1 downto 0 );
+    DIRECTIONS3_tri_o : out STD_LOGIC_VECTOR ( 1 downto 0 )
+  );
+  end component design_1;
+  component IOBUF is
+  port (
+    I : in STD_LOGIC;
+    O : out STD_LOGIC;
+    T : in STD_LOGIC;
+    IO : inout STD_LOGIC
+  );
+  end component IOBUF;
+  signal IIC_0_0_scl_i : STD_LOGIC;
+  signal IIC_0_0_scl_o : STD_LOGIC;
+  signal IIC_0_0_scl_t : STD_LOGIC;
+  signal IIC_0_0_sda_i : STD_LOGIC;
+  signal IIC_0_0_sda_o : STD_LOGIC;
+  signal IIC_0_0_sda_t : STD_LOGIC;
+  signal IIC_0_scl_i : STD_LOGIC;
+  signal IIC_0_scl_o : STD_LOGIC;
+  signal IIC_0_scl_t : STD_LOGIC;
+  signal IIC_0_sda_i : STD_LOGIC;
+  signal IIC_0_sda_o : STD_LOGIC;
+  signal IIC_0_sda_t : STD_LOGIC;
+begin
+IIC_0_0_scl_iobuf: component IOBUF
+     port map (
+      I => IIC_0_0_scl_o,
+      IO => IIC_0_0_scl_io,
+      O => IIC_0_0_scl_i,
+      T => IIC_0_0_scl_t
+    );
+IIC_0_0_sda_iobuf: component IOBUF
+     port map (
+      I => IIC_0_0_sda_o,
+      IO => IIC_0_0_sda_io,
+      O => IIC_0_0_sda_i,
+      T => IIC_0_0_sda_t
+    );
+IIC_0_scl_iobuf: component IOBUF
+     port map (
+      I => IIC_0_scl_o,
+      IO => IIC_0_scl_io,
+      O => IIC_0_scl_i,
+      T => IIC_0_scl_t
+    );
+IIC_0_sda_iobuf: component IOBUF
+     port map (
+      I => IIC_0_sda_o,
+      IO => IIC_0_sda_io,
+      O => IIC_0_sda_i,
+      T => IIC_0_sda_t
+    );
+design_1_i: component design_1
+     port map (
+      DDR_addr(14 downto 0) => DDR_addr(14 downto 0),
+      DDR_ba(2 downto 0) => DDR_ba(2 downto 0),
+      DDR_cas_n => DDR_cas_n,
+      DDR_ck_n => DDR_ck_n,
+      DDR_ck_p => DDR_ck_p,
+      DDR_cke => DDR_cke,
+      DDR_cs_n => DDR_cs_n,
+      DDR_dm(3 downto 0) => DDR_dm(3 downto 0),
+      DDR_dq(31 downto 0) => DDR_dq(31 downto 0),
+      DDR_dqs_n(3 downto 0) => DDR_dqs_n(3 downto 0),
+      DDR_dqs_p(3 downto 0) => DDR_dqs_p(3 downto 0),
+      DDR_odt => DDR_odt,
+      DDR_ras_n => DDR_ras_n,
+      DDR_reset_n => DDR_reset_n,
+      DDR_we_n => DDR_we_n,
+      DIRECTIONS0_tri_o(1 downto 0) => DIRECTIONS0_tri_o(1 downto 0),
+      DIRECTIONS1_tri_o(1 downto 0) => DIRECTIONS1_tri_o(1 downto 0),
+      DIRECTIONS2_tri_o(1 downto 0) => DIRECTIONS2_tri_o(1 downto 0),
+      DIRECTIONS3_tri_o(1 downto 0) => DIRECTIONS3_tri_o(1 downto 0),
+      FIXED_IO_ddr_vrn => FIXED_IO_ddr_vrn,
+      FIXED_IO_ddr_vrp => FIXED_IO_ddr_vrp,
+      FIXED_IO_mio(53 downto 0) => FIXED_IO_mio(53 downto 0),
+      FIXED_IO_ps_clk => FIXED_IO_ps_clk,
+      FIXED_IO_ps_porb => FIXED_IO_ps_porb,
+      FIXED_IO_ps_srstb => FIXED_IO_ps_srstb,
+      IIC_0_0_scl_i => IIC_0_0_scl_i,
+      IIC_0_0_scl_o => IIC_0_0_scl_o,
+      IIC_0_0_scl_t => IIC_0_0_scl_t,
+      IIC_0_0_sda_i => IIC_0_0_sda_i,
+      IIC_0_0_sda_o => IIC_0_0_sda_o,
+      IIC_0_0_sda_t => IIC_0_0_sda_t,
+      IIC_0_scl_i => IIC_0_scl_i,
+      IIC_0_scl_o => IIC_0_scl_o,
+      IIC_0_scl_t => IIC_0_scl_t,
+      IIC_0_sda_i => IIC_0_sda_i,
+      IIC_0_sda_o => IIC_0_sda_o,
+      IIC_0_sda_t => IIC_0_sda_t,
+      PWM0 => PWM0,
+      PWM1 => PWM1,
+      PWM2 => PWM2,
+      PWM3 => PWM3,
+      clk_in1_0 => clk_in1_0,
+      signal_in_0 => signal_in_0,
+      signal_in_1 => signal_in_1,
+      signal_in_2 => signal_in_2,
+      signal_in_3 => signal_in_3,
+      sonar_echo_0 => sonar_echo_0,
+      sonar_echo_1 => sonar_echo_1,
+      sonar_trig_0 => sonar_trig_0,
+      sonar_trig_1 => sonar_trig_1
+    );
+end STRUCTURE;
